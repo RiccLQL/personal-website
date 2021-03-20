b63ac93897a04bd11a9c68e6df35f4b04c88a687
Ricky Liu <li.qi.liu@mail.mcgill.ca>
2021-03-20 02:46:04 -0400

    changed name of site

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'personal-website',
+    title: 'Ricky Liu - Personal Website',
     htmlAttrs: {
       lang: 'en'
     },
