c8badb29c9ec1f8560568675fb9426d7ff761540
Ricky Liu <li.qi.liu@mail.mcgill.ca>
2021-03-22 12:27:09 -0400

    donezo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Ricky Liu - Personal Website',
+    title: 'Ricky Liu - Software Engineering @ McGill University',
     htmlAttrs: {
       lang: 'en'
     },
